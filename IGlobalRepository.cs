d9307f5c4fa6a8e5493d78f6bc839d893f930dc2
FakeSe <mabroukifakhri@gmail.com>
2018-07-05 15:01:58 +0200

    Update IGlobalRepository.cs

--- IGlobalRepository.cs
+++ IGlobalRepository.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace ImmoSales.Repositories.Repositories.Global
+namespace YourProject.Repositories.Global
 {
     public interface IGlobalRepository
     {
